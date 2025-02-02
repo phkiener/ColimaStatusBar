99e8247176204e8df88934f7563a25450e216c8b
Philipp Kiener <kiener.phil@gmail.com>
2025-02-02 14:46:54 +0100

    Display icon and toggle

--- ColimaStatusBar/ColimaStatusBarApp.swift
+++ ColimaStatusBar/ColimaStatusBarApp.swift
@@ -2,9 +2,25 @@ import SwiftUI
 
 @main
 struct ColimaStatusBarApp: App {
+    @State var running: Bool = false;
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Colima Status", systemImage: running ? "shippingbox.fill" : "shippingbox") {
+            if (running) {
+                Button("Stop") {
+                    running = false;
+                }
+            }
+            
+            if (!running) {
+                Button("Start") {
+                    running = true;
+                }
+            }
+            
+            Button("Exit") {
+                exit(EXIT_SUCCESS);
+            }
         }
     }
 }
