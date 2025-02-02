298b39fbca32c69ffdb8d6580b1638a6c6a81071
Philipp Kiener <kiener.phil@gmail.com>
2025-02-02 14:28:37 +0100

    Initial Commit

---
+++ ColimaStatusBar/ColimaStatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColimaStatusBarApp.swift
+//  ColimaStatusBar
+//
+//  Created by Phil on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColimaStatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColimaStatusBarTests/ColimaStatusBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColimaStatusBarTests.swift
+//  ColimaStatusBarTests
+//
+//  Created by Phil on 02.02.2025.
+//
+
+import Testing
+@testable import ColimaStatusBar
+
+struct ColimaStatusBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
