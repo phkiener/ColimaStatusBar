084884e51e998bb47093db871f3c0f85ced7d273
Philipp Kiener <kiener.phil@gmail.com>
2025-02-09 16:18:39 +0100

    Use System status bar

--- src/ColimaStatusBar.Native/AppDelegate.cs
+++ src/ColimaStatusBar.Native/AppDelegate.cs
@@ -9,19 +9,17 @@ public sealed class AppDelegate : NSApplicationDelegate
 
     private readonly List<Subscription> subscriptions = [];
     private ColimaInteractor interactor = null!;
-    private NSStatusBar? statusBar;
     private NSStatusItem? statusItem;
     private NSMenu? statusBarMenu;
     private NSMenuItem? status;
-    
+
     public override void DidFinishLaunching(NSNotification notification)
     {
         interactor = new ColimaInteractor(TimeSpan.FromSeconds(1));
         interactor.PropertyChanged += Observe;
         Observe(interactor, i => i.IsRunning, UpdateStatus);
-
-        statusBar = new NSStatusBar();
-        statusItem = statusBar.CreateStatusItem(NSStatusItemLength.Square);
+        
+        statusItem = NSStatusBar.SystemStatusBar.CreateStatusItem(NSStatusItemLength.Variable);
         statusItem.Button.Image = NSImage.GetSystemSymbol("shippingbox", accessibilityDescription: null);
         
         statusBarMenu = new NSMenu();
@@ -43,7 +41,6 @@ public sealed class AppDelegate : NSApplicationDelegate
             
             statusBarMenu?.Dispose();
             statusItem?.Dispose();
-            statusBar?.Dispose();
         }
     }
 
@@ -53,7 +50,7 @@ public sealed class AppDelegate : NSApplicationDelegate
         foreach (var matchingSubscription in matchingSubscriptions)
         {
             var value = matchingSubscription.Receiver(sender!);
-            matchingSubscription.Handle(value);
+            InvokeOnMainThread(() => matchingSubscription.Handle(value));
         }
     }
 
@@ -72,7 +69,12 @@ public sealed class AppDelegate : NSApplicationDelegate
     {
         if (status is not null)
         {
-            status.Title = interactor.IsRunning ? "Running" : "Stopped";
+            status.Title = isRunning ? "Running" : "Stopped";
+        }
+
+        if (statusItem is not null)
+        {
+            statusItem.Button.Image = NSImage.GetSystemSymbol(isRunning ? "shippingbox.fill" : "shippingbox", accessibilityDescription: null);
         }
     }
 
