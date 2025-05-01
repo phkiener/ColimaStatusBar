498b6c962a421fc079b14d39de1de2b33685bc07
Philipp Kiener <kiener.phil@gmail.com>
2025-05-01 16:23:18 +0200

    Remove old code

--- src/Reactive/ISubscription.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-
-namespace Reactive;
-
-public interface ISubscription<TObservable, out TValue> where TObservable : INotifyPropertyChanged
-{
-    ISubscription<TObservable, TValue> Handle(Action<TValue> handler);
-    ISubscription<TObservable, TValue> Decorate(Action<Action> decorator);
-}
--- src/Reactive/Observer.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-using Reactive.Implementation;
-
-namespace Reactive;
-
-public static class Observer
-{
-    public static IObserver<T> Create<T>(T value) where T : INotifyPropertyChanged => new DefaultObserver<T>(value);
-}
